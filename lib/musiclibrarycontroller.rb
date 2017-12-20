3ab7d6d14336ca227bc09eda98febbab1a998d84
Phoebe Lau <phoebelau7@gmail.com>
2017-12-20 03:32:28 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -51,7 +51,7 @@ class MusicLibraryController
 
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
-    user_input = gets
+    user_input = gets.strip
     artist = Artist.find_by_name(user_input)
     artist == nil ? nil :
       artist.songs.sort_by {|songs|songs.name}.each_with_index{|song,index|
